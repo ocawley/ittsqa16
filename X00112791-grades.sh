7035cf8def2cb565b4b7db7cbdf602aa2aa56edc
X00112791 <X00112791@gmail.com>
2014-10-28 15:23:29 +0000

    Create X00112791-grades.sh

---
+++ X00112791-grades.sh
@@ -0,0 +1,31 @@
+#Input: 20 20
+#Expected Output: fail
+#Actual Output: fail
+java grades 20 20
+
+#Input: 35 80
+#Expected Output: component fail
+#Actual Output: component fail
+java grades 35 80
+
+#Input: 60 20
+#Expected Output: component fail
+#Actual Output: component fail
+java grades 60 20
+
+#Input: 70 70
+#Expected Output: pass
+#Actual Output: pass
+java grades 70 70
+
+#Input: 55 60
+#Expected Output:pass
+#Actual Output: fail
+java grades 55 60
+
+#Input: 90 90
+#Expected Output:pass with distinction
+#Actual Output: pass with distinction
+java grades 90 90
+
+sleep 10m
